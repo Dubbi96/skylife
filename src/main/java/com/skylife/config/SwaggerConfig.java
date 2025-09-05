2028e0b8dabee2dcc7098a3cc87afc47e3470d85
Dubbi96 <yrs03001@hanyang.ac.kr>
2025-09-05 15:34:53 +0900

    chore : clean up doc

--- src/main/java/com/skylife/config/SwaggerConfig.java
+++ src/main/java/com/skylife/config/SwaggerConfig.java
@@ -3,12 +3,7 @@ package com.skylife.config;
 import io.swagger.v3.oas.annotations.OpenAPIDefinition;
 import io.swagger.v3.oas.annotations.info.Info;
 import io.swagger.v3.oas.annotations.servers.Server;
-import io.swagger.v3.oas.models.Components;
 import io.swagger.v3.oas.models.OpenAPI;
-import io.swagger.v3.oas.models.security.SecurityRequirement;
-import io.swagger.v3.oas.models.security.SecurityScheme;
-import io.swagger.v3.oas.models.security.SecurityScheme.In;
-import io.swagger.v3.oas.models.security.SecurityScheme.Type;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
